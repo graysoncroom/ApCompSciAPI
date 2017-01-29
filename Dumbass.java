1e4f934d7376504aaf6009c1ee697ff493a1509d
Grayson Croom <grayson.croom@gmail.com>
2017-01-29 17:10:51 -0600

    Create Dumbass.java

---
+++ Dumbass.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import lib.*;
+
+/**
+ * @author Grayson Croom
+ *
+ */
+public class Dumbass {
+	
+	/**
+	 * 
+	 * @param fileName
+	 * @param lineNumber
+	 * @return the text (the whole line) at 'lineNumber' in file 'fileName'
+	 */
+	public static String readFile(String fileName, int lineNumber) { 
+		return (new FileReader(fileName)).readLine(lineNumber);
+	}
+
+	@SuppressWarnings("resource")
+	public static String readStringFromConsole(){
+		return (new Scanner( System.in )).nextLine();
+	}
+	
+	@SuppressWarnings("resource")
+	public static int readIntFromConsole(){
+		return (new Scanner( System.in )).nextInt();
+	}
+	
+	@SuppressWarnings("resource")
+	public static char readCharFromConsole(){
+		return ((new Scanner( System.in )).next()).charAt(0);
+	}
+
+}
