4fb690fd860f5764e6da82d4b65499d2f31fa587
Grayson Croom <grayson.croom@gmail.com>
2017-01-29 22:40:15 -0600

    Update tester.java

--- tester.java
+++ tester.java
@@ -1,22 +1,24 @@
 public class tester {
 	public static void main(String[] args) {
 		
-		System.out.println("testing Dumbass.printRightTriangle(\"top left\", 10, \'*\') \n");
+		System.out.println();
+		
+		System.out.println("testing Dumbass.printRightTriangle(\"top left\", 10, \'*\')");
 		Dumbass.printRightTriangle("top left", 10, '*');
 		
 		System.out.println();
 		
-		System.out.println("testing Dumbass.printRightTriangle(\"bottom left\", 10, \'*\') \n");
+		System.out.println("testing Dumbass.printRightTriangle(\"bottom left\", 10, \'*\')");
 		Dumbass.printRightTriangle("bottom left", 10, '*');
 		
 		System.out.println();
 		
-		System.out.println("testing Dumbass.stringToInt(\"45\") \n");
+		System.out.println("testing Dumbass.stringToInt(\"45\")");
 		System.out.println( Dumbass.stringToInt("45") );
 		
 		System.out.println();
 		
-		System.out.println("testing Dumbass.readStringFromConsole() \n");
+		System.out.println("testing Dumbass.readStringFromConsole()");
 		System.out.println( Dumbass.readStringFromConsole() );
 		
 		System.out.println();
